f2aed2626ac56065bf3e4c661063079f048e31fc
Maria Selivanova <maria.m.selivanova@yandex.ru>
2022-10-10 20:27:52 +0600

    15ПР

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -10,19 +10,21 @@ const cors = (req, res, next) => {
   const { origin } = req.headers;
   console.log(origin);
   const { method } = req;
+  console.log(method);
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (allowedCors.includes(origin)) {
-    console.log('hehehe');
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
   if (method === 'OPTIONS') {
     console.log('hфрфрф');
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+  }
+  if (allowedCors.includes(origin)) {
+    console.log('hehehe');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
     return res.end();
   }
+
   return next();
 };
 module.exports = cors;
