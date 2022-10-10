a322276f6bf4f4d619d51e1edd171a764287cb88
Maria Selivanova <maria.m.selivanova@yandex.ru>
2022-10-10 15:40:00 +0600

    15ПР

--- app.js
+++ app.js
@@ -50,13 +50,21 @@ app.all('*', (req, res, next) => {
 });
 app.use(errorLogger);
 app.use(errors());
+
 app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
+  const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
   res.header('Access-Control-Allow-Origin', '*');
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
   next();
 });
 
