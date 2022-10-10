c3040d6a0f84ab73e37208e0520593be4dd9d44b
Maria Selivanova <maria.m.selivanova@yandex.ru>
2022-10-10 21:01:33 +0600

    15ПР

--- controllers/users.js
+++ controllers/users.js
@@ -17,12 +17,6 @@ const login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
       res.send({ token });
-      res.cookie('token', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-        sameSite: 'none',
-        secure: true,
-      });
     })
     .catch(() => next(new AuthError('Неверные почта или пароль')));
 };
