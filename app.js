e40c6bb1dc00b988d5327139dbdd3938d5c6d10d
Maria Selivanova <maria.m.selivanova@yandex.ru>
2022-10-10 15:16:17 +0600

    15ПР

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const routerUser = require('./routes/users');
 const routerCard = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
@@ -14,18 +15,7 @@ const {
   loginValidation,
   userValidation,
 } = require('./middlewares/validation');
-const allowedCors = [
-  'https://mestoproject.nomoredomains.icu',
-  'http://mestoproject.nomoredomains.icu',
-  'https://api.mestoproject.nomoredomains.icu',
-  'http://api.mestoproject.nomoredomains.icu',
-  'https://www.api.mestoproject.nomoredomains.icu',
-  'http://www.api.mestoproject.nomoredomains.icu',
-  'http://localhost:3000',
-  'https://localhost:3000',
-  'http://localhost:3001',
-  'https://localhost:3001',
-]
+
 const NotFoundError = require('./errors/not-found-err');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -36,15 +26,22 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin: [
+    'https://mestoproject.nomoredomains.icu',
+    'http://mestoproject.nomoredomains.icu',
+    'https://api.mestoproject.nomoredomains.icu',
+    'http://api.mestoproject.nomoredomains.icu',
+    'https://www.api.mestoproject.nomoredomains.icu',
+    'http://www.api.mestoproject.nomoredomains.icu',
+    'http://localhost:3000',
+    'https://localhost:3000',
+    'http://localhost:3001',
+    'https://localhost:3001',
+  ],
+  credentials: true,
+}));
 app.use(requestLogger);
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 app.post('/signin', loginValidation, login);
 app.post('/signup', userValidation, createUser);
 app.use(auth);
